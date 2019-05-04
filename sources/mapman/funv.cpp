fc1a71519efd77b0435e255a152ae389308e0ebc
akosbalogh01 <akos.balogh01@gmail.com>
2019-05-04 04:50:33 +0200

    init revamp with new parser

---
+++ sources/mapman/funv.cpp
@@ -0,0 +1,10 @@
+#include "statfunc.hpp"
+#include "mapman.hpp"
+#include "instrvector.hpp"
+
+std::map <std::string, games::instrvector> games::mapman::funvmap;
+
+void games::mapman::init_funvmap() {
+
+}
+
