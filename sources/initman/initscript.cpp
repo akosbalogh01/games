5855d6dfe6c28b24a4fd69d88762fab8023228d6
akosbalogh01 <akos.balogh01@gmail.com>
2019-05-17 03:52:52 +0200

    command processor refactor

--- sources/initman/initscript.cpp
+++ sources/initman/initscript.cpp
@@ -9,9 +9,7 @@
 
 bool games::initman::loadInitScript(const std::string& filepath) {
     std::cout << "[i] Generating init script from " << filepath << ':' << std::endl;
-    games::mapman::varimap["LOFASZ"];
-    games::mapman::varimap["KUTYAF"];
-    //games::mapman::funvmap["INIT"];
+
     vinitscript = games::scriptman::lexer(filepath);
     vinstvector = games::scriptman::parser(vinitscript);
     games::mapman::funvmap["INIT"] = vinstvector;
